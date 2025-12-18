66b7b1cf991c4802e0c2bf4e9cbe81b5f40f5e38
levar1 <ravelsilveiradamacedo@gmail.com>
2025-12-18 11:06:12 -0300

    Refactor smooth scroll functionality for internal links

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Scroll suave ao clicar em links internos
 document.querySelectorAll('a[href^="#"]').forEach(link => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
-    const alvo = document.querySelector(this.getAttribute("href"));
-    alvo.scrollIntoView({ behavior: "smooth" });
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
   });
 });
